198e0391c75d23301a520b0ddc23a970ee384489
Alexandre WEISSER <alexandre.weisser@gmail.com>
2022-03-29 18:56:19 +0200

    feat : secret secrets

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,11 @@
-ECHO 1.1.0
+ECHO 1.1.1
 
-scp dist\wordwave.1.1.0.tar.gz pazka@hosh.it:~/serious/WordWave
+scp dist\wordwave.1.1.1.tar.gz pazka@hosh.it:~/serious/WordWave
 ssh pazka@hosh.it
 
 cd ~/serious/WordWave
-gzip -d wordwave.1.1.0.tar.gz
+gzip -d wordwave.1.1.1.tar.gz
 docker stop WordWave && docker rm WordWave || docker rm WordWave 
-docker load < wordwave.1.1.0.tar
+docker load < wordwave.1.1.1.tar
 
 docker run -d -it -p 9898:80 --name WordWave -e LOGIN=<your_login> -e MDP=<your_mdp> -e secret=<your_secret> wordwave
